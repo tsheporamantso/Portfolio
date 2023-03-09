c4d7c66cd5aae9057c1ff35ca0b2177109c0bf49
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-09 16:52:03 +0200

    Add JS function for local storage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav__menu__mobile');
 
@@ -11,3 +12,21 @@ document.querySelectorAll('.dt__link').forEach((n) => n
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
   }));
+
+const fullName = document.querySelector('#username');
+const feedback = document.querySelector('.msg');
+function visitorData() {
+  const visitor = {
+    Name: fullName.value,
+    Email: email.value,
+    Message: feedback.value,
+  };
+  localStorage.setItem('visitor', JSON.stringify(visitor));
+}
+[fullName, email, feedback].forEach((input) => input.addEventListener('focusout', visitorData));
+const visitorDataExist = JSON.parse(localStorage.getItem('visitor'));
+if (visitorDataExist) {
+  fullName.value = visitorDataExist.Name;
+  email.value = visitorDataExist.Email;
+  feedback.value = visitorDataExist.Message;
+}
